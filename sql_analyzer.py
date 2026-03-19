36769ce4ad2b2a86c832560f2e94606d68383a3f
PauloWg <paulo.waimberg@familiar.com.py>
2026-03-19 08:07:48 -0300

    Extract Joins from query

--- sql_analyzer.py
+++ sql_analyzer.py
@@ -4,9 +4,7 @@ import numpy as np
 import sqlglot
 from sqlglot import exp
 
-st.title('Analizador de Query SQL')
-
-query = st.text_area('Insert Query here...')
+###   U T I L S 
 
 def extract_conditions(expression):
     conditions = []
@@ -30,6 +28,24 @@ def extract_selects(expression):
 
     return selects
 
+def extract_joins(select):
+    joins = []
+    for join in select.find_all(exp.Join):
+        join_type = join.args.get("kind", "INNER").upper()  # Default to INNER if not specified
+        join_table = join.this.sql()
+        on_conditions = []
+
+        # Extract ON conditions if they exist
+        if join.args.get("on"):
+            on_conditions = extract_conditions(join.args["on"])
+
+        joins.append({
+            "table": join_table,
+            "type": join_type,
+            "on_conditions": on_conditions
+        })
+    return joins
+
 
 def analyze_query(query):
     try:
@@ -44,17 +60,16 @@ def analyze_query(query):
         conditions = []
 
         for select in selects:
-            columns = []
-            tables = []
             conditions = []
 
             # Columns
-            for col in select.find_all(exp.Column):
-                columns.append(col.sql())
+            columns = list(dict.fromkeys(col.sql() for col in select.find_all(exp.Column)))
 
             # Tables
-            for table in select.find_all(exp.Table):
-                tables.append(table.sql())
+            tables = list(dict.fromkeys(table.sql() for table in select.find_all(exp.Table)))
+
+            # JOINS
+            joins = extract_joins(select)
 
             # WHERE
             where = select.find(exp.Where)
@@ -65,7 +80,8 @@ def analyze_query(query):
                 "query": select.sql(pretty=True),
                 "columns": columns,
                 "tables": tables,
-                "conditions": conditions
+                "conditions": conditions,
+                "joins": joins
             })
 
         return parsed, results
@@ -73,6 +89,12 @@ def analyze_query(query):
     except Exception as e:
         return [{"error": str(e)}]
 
+
+###  D I S P L A Y   P Á G I N A 
+
+st.title('Analizador de Query SQL')
+
+query = st.text_area('Insert Query here...')
 
 if query:
     full_query, results = analyze_query(query)
@@ -94,6 +116,12 @@ if query:
 
         st.subheader("Tables")
         st.dataframe(pd.DataFrame(res["tables"], columns=["Table"]))
+
+        st.subheader("Joins")
+        join_data = []
+        for j in res["joins"]:
+            join_data.append([j["type"], j["table"], ", ".join(j["on_conditions"])])
+        st.dataframe(pd.DataFrame(join_data, columns=["Join Type", "Table", "ON Conditions"]))
 
         st.subheader("Conditions")
         st.dataframe(pd.DataFrame(res["conditions"], columns=["Condition"]))
